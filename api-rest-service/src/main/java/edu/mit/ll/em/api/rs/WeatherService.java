e85fab3640fdb4f62964958b43ff1546d956316b
snagole <sneha.nagole@tabordasolutions.com>
2018-09-24 15:06:07 -0700

    Changes to add RS Path annotations

--- api-rest-service/src/main/java/edu/mit/ll/em/api/rs/WeatherService.java
+++ api-rest-service/src/main/java/edu/mit/ll/em/api/rs/WeatherService.java
@@ -1,8 +1,7 @@
 package edu.mit.ll.em.api.rs;
 
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/weather")
@@ -10,7 +9,9 @@ public interface WeatherService {
     public static String DEFAULT_SEARCH_RANGE_IN_MILES = "10.0";
     public static String DEFAULT_SOURCE_CRS = "EPSG:3857";
 
-    public APIResponse getWeatherData(@QueryParam("latitude") Double latitude, @QueryParam("longitude") Double longitude,
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public APIResponse getWeatherData(@QueryParam("longitude") Double longitude, @QueryParam("latitude") Double latitude,
                                    @DefaultValue(DEFAULT_SOURCE_CRS) @QueryParam("CRS") String locationCRS,
                                    @DefaultValue(DEFAULT_SEARCH_RANGE_IN_MILES) @QueryParam("searchRangeInMiles") Double searchRangeInMiles);
 }
