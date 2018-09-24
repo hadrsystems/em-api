f25e25fb0e706cbdb42115892844d28a4f2049a8
snagole <sneha.nagole@tabordasolutions.com>
2018-09-24 15:15:03 -0700

    Changes to weatherDao bean definition issues

--- api-rest-service/src/main/java/edu/mit/ll/em/api/configuration/SpringConfiguration.java
+++ api-rest-service/src/main/java/edu/mit/ll/em/api/configuration/SpringConfiguration.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2008-2016, Massachusetts Institute of Technology (MIT)
+ * Copyright (c) 2008-2018, Massachusetts Institute of Technology (MIT)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -143,7 +143,7 @@ public class SpringConfiguration {
     }
 
     @Bean
-    WeatherDAO weatherDAO() throws NamingException {
+    WeatherDAO weatherDao() throws NamingException {
         return new WeatherDAOImpl(this.dataFeedsJdbcTemplate());
     }
 
